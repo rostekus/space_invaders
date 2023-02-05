0fc8db02a6042c4219e46bf1d760719f85f65903
Rostyslav Mosorov <rostyslavmosorov@gmail.com>
2023-02-05 14:13:51 +0100

    init main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,85 +19,80 @@
 #include <iostream>
 int main()
 {
-    std::cout<< "hello" << std::endl;
-    Projectile p(true);
-    Player p1;
-    Alien e1;
-
     Board *board = new Board();
-//    int round = 0;
-//
-//    // Create a player and add it to the board
-//    Player *player = new Player(floor(board->getWidth() / 2), board->getHeight() - 1);
-//    board->addObject(player);
-//
-//    // Create aliens and add to board
-//    int midSpace = floor(board->getWidth() / 2);
-//    int numRows = 2;
-//    int numCols = 4;
-//    for (int i = 0; i < numRows; i++)
-//    {
-//        for (int j = 0; j < numCols; j++)
-//        {
-//            // don't know why we have to add this pointer to generate correct # of aliens but it breaks otherwise
-//            Alien *alien = new Alien(midSpace - (numCols / 2) + j, i + 1);
-//            board->addObject(new Alien(midSpace - (numCols / 2) + j, i + 1));
-//        }
-//    }
-//
-//    while (true)
-//    {
-//        // Check if game is over
-//        if (player->getLives() <= 0)
-//        {
-//            board->writeMessage("Game Over.");
-//        }
-//        else if (board->getObjects(ALIEN_REP).size() == 0)
-//        {
-//            round++;
-//            for (int i = 0; i < numRows; i++)
-//            {
-//                for (int j = 0; j < numCols; j++)
-//                {
-//                    Alien *alien = new Alien(midSpace - (numCols / 2) + j, i + 1, round);
-//                    board->addObject(new Alien(midSpace - (numCols / 2) + j, i + 1, round));
-//                }
-//            }
-//        }
-//        else
-//        {
-//            // Player loses life when aliens hit bottom
-//            auto aliens = board->getObjects(ALIEN_REP);
-//            for (auto &alien : aliens)
-//            {
-//                if (alien->getPosY() > player->getPosY())
-//                {
-//                    player->loseLife();
-//                }
-//            }
-//        }
-//
-//        board->update();
-//        // Check for user input
-//        int c = board->getInput();
-//        switch (c)
-//        {
-//        case RIGHT_KEY:
-//            if (player->getPosX() < board->getWidth() - 1)
-//            {
-//                player->moveRight();
-//            }
-//            break;
-//        case LEFT_KEY:
-//            if (player->getPosX() > 1)
-//            {
-//                player->moveLeft();
-//            }
-//            break;
-//        case UP_KEY:
-//            Projectile *p = new Projectile(true, player->getPosX(), player->getPosY() - 1);
-//            board->addObject(new Projectile(true, player->getPosX(), player->getPosY() - 1));
-//            break;
-//        }
-//    }
+    int round = 0;
+
+    // Create a player and add it to the board
+    Player *player = new Player(floor(board->getWidth() / 2), board->getHeight() - 1);
+    board->addObject(player);
+
+    // Create aliens and add to board
+    int midSpace = floor(board->getWidth() / 2);
+    int numRows = 2;
+    int numCols = 4;
+    for (int i = 0; i < numRows; i++)
+    {
+        for (int j = 0; j < numCols; j++)
+        {
+            // don't know why we have to add this pointer to generate correct # of aliens but it breaks otherwise
+            Alien *alien = new Alien(midSpace - (numCols / 2) + j, i + 1);
+            board->addObject(new Alien(midSpace - (numCols / 2) + j, i + 1));
+        }
+    }
+
+    while (true)
+    {
+        // Check if game is over
+        if (player->getLives() <= 0)
+        {
+            board->writeMessage("Game Over.");
+        }
+        else if (board->getObjects(ALIEN_REP).size() == 0)
+        {
+            round++;
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    Alien *alien = new Alien(midSpace - (numCols / 2) + j, i + 1, round);
+                    board->addObject(new Alien(midSpace - (numCols / 2) + j, i + 1, round));
+                }
+            }
+        }
+        else
+        {
+            // Player loses life when aliens hit bottom
+            auto aliens = board->getObjects(ALIEN_REP);
+            for (auto &alien : aliens)
+            {
+                if (alien->getPosY() > player->getPosY())
+                {
+                    player->loseLife();
+                }
+            }
+        }
+
+        board->update();
+        // Check for user input
+        int c = board->getInput();
+        switch (c)
+        {
+        case RIGHT_KEY:
+            if (player->getPosX() < board->getWidth() - 1)
+            {
+                player->moveRight();
+            }
+            break;
+        case LEFT_KEY:
+            if (player->getPosX() > 1)
+            {
+                player->moveLeft();
+            }
+            break;
+        case UP_KEY:
+            Projectile *p = new Projectile(true, player->getPosX(), player->getPosY() - 1);
+            board->addObject(new Projectile(true, player->getPosX(), player->getPosY() - 1));
+            break;
+        }
+    }
 }
