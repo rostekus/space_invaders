e127e47d7387990332118fcbf2b0c52c08f42124
Rostyslav Mosorov <rostyslavmosorov@gmail.com>
2023-02-08 17:19:51 +0100

    fix: testEnemyHitPlayer

--- test/TestGame.cpp
+++ test/TestGame.cpp
@@ -6,18 +6,18 @@
 
 BOOST_AUTO_TEST_SUITE(TestGame)
 
-BOOST_AUTO_TEST_CASE(testEnemyHitUser)
+BOOST_AUTO_TEST_CASE(testEnemyHitPlayer)
 {
     Game *game = new Game();
     game->addPlayerToBoard();
-    int posXEnemy = floor(game->getBoard()->getWidth() / 2);
-    int posYEnemy = game->getBoard()->getHeight() - 1;
+    int posXEnemy = game->getPlayer()->getposX();
+    int posYEnemy = game->getPlayer()->getposY() + 1;
     game->getBoard()->addObject(new Enemy(posXEnemy, posYEnemy));
     game->checkAlienHitPlayer();
-    BOOST_CHECK_EQUAL(DEF_LIVES, game->getPlayer()->getLives());
+    BOOST_CHECK_EQUAL(DEF_LIVES - 1, game->getPlayer()->getLives());
 }
 
-BOOST_AUTO_TEST_CASE(testGameOver)
+BOOST_AUTO_TEST_CASE(testBreakLoopWhenGameOver)
 {
     Game *game = new Game();
     game->addPlayerToBoard();
@@ -26,7 +26,7 @@ BOOST_AUTO_TEST_CASE(testGameOver)
         game->getPlayer()->loseLife();
     }
     game->runGame();
-    BOOST_CHECK_EQUAL(game->isGameOver, true);
+    BOOST_CHECK_EQUAL(game->checkGameOver(), true);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
